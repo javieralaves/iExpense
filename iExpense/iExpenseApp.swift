a5a4c2a4853ff94f87e15605b941b96a2ee31c3d
Javier Alaves <jayalaves@gmail.com>
2023-07-12 19:49:34 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Javier Alaves on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
