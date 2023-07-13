5555675995d40f725ee0fbad8adbdae954e20bea
Javier Alaves <jayalaves@gmail.com>
2023-07-13 11:50:05 +0200

    Data management

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -32,6 +32,12 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("New expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
         }
     }
 }
